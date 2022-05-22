b6e2bdacbcabb065df98a7422fdbe391c9fd570d
vesa193 <veselinovic93@hotmail.com>
2022-05-23 00:04:17 +0200

    remove unused file, rename title of the app, modify style files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,22 @@
 import PropTypes from 'prop-types';
+import { Link, useLocation } from 'react-router-dom';
 import Button from '../Button/Button';
 import './Header.css';
 
 export default function Header({ handleRefreshData, isDisableButton }) {
+    const location = useLocation();
+    const hasSearchQuery = location?.search;
+
     return (
         <header className="header">
             <nav>
-                <b>HackerNews</b>
+                {hasSearchQuery ? (
+                    <Link className="header__logo" to="/stories">
+                        <b>HackerNews</b>
+                    </Link>
+                ) : (
+                    <b>HackerNews</b>
+                )}
                 <Button isDisabled={isDisableButton} label="Refresh data" handleButton={handleRefreshData} />
             </nav>
         </header>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
-import './index.css';
 import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
 
