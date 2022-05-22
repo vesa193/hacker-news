d28ae8677fd6013f7e104aeef9c69a14128b5848
vesa193 <veselinovic93@hotmail.com>
2022-05-22 23:33:26 +0200

    Remove current unused files, remove basename attribute from HashRouter

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Router basename={`/${process.env.PUBLIC_URL}`}>
+        <Router>
             <Provider store={store}>
                 <App />
             </Provider>
